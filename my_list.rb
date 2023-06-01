6a7581d48593eb9442addd40d1c576e2b5f08654
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-06-01 18:37:17 +0430

    any? enumerable function added

--- my_list.rb
+++ my_list.rb
@@ -4,5 +4,13 @@ module MyEnum
           return false unless yield x
         end
         true
+    end
+
+    def any?
+        each do |x|
+          return true if yield x
+        end
+        false
       end
+      
 end
