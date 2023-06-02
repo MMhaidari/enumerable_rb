217c6f3dfe8dbfc5073e2ac5049d935e4019bb35
dieum512 <akonkwadieum222@gmail.com>
2023-06-02 14:03:17 +0200

    Correct the linter errors

--- spec/my_list.rb
+++ spec/my_list.rb
@@ -1,16 +1,16 @@
-require_relative './my_enumeable' 
-class MyList 
-  def initialize(*arr) 
-    @list = arr 
-  end 
-  include MyEnum 
-  def each(&block) 
-    @list.each { |x| block.call(x) } 
-  end 
-end 
-list = MyList.new(1, 2, 3, 4) 
-puts(list.all? { |e| e < 5 }) 
-puts(list.all? { |e| e > 5 }) 
-puts(list.any? { |e| e == 2 }) 
-puts(list.any? { |e| e == 5 }) 
-puts(list.filter(&:even?)) 
+require_relative './my_enumeable'
+class MyList
+  def initialize(*arr)
+    @list = arr
+  end
+  include MyEnum
+  def each(&block)
+    @list.each { |x| block.call(x) }
+  end
+end
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter(&:even?))
